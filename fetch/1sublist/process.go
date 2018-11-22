e17657a629a65c9372167e13effda20e41876f02
siongui <siongui@gmail.com>
2018-11-23 06:21:13 +0800

    move type code to another file

--- fetch/1sublist/process.go
+++ fetch/1sublist/process.go
@@ -7,49 +7,6 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-type productItem struct {
-	Title     string
-	Href      string
-	ImageSrcs []string
-}
-
-type productList struct {
-	RstPath string
-	Title   string
-	Url     string
-	Items   []productItem
-}
-
-func (l *productList) SetUrlTitle() {
-	_, err := replaceLines(l.RstPath, func(s string) string {
-		if strings.HasPrefix(s, ":title: ") {
-			l.Title = strings.TrimPrefix(s, ":title: ")
-		}
-		if strings.HasPrefix(s, ":source: ") {
-			l.Url = strings.TrimPrefix(s, ":source: ")
-		}
-		return s
-	})
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (l productList) CreateFinalProductRstFiles() {
-	fmt.Println()
-	for _, item := range l.Items {
-		ogImg := getRstImagePath(item.ImageSrcs[0])
-
-		s := rstMeta(item.Title, titleToSlug(item.Title),
-			"product, "+l.Title,
-			item.Title+" - "+l.Title,
-			"en", "", item.Href, ogImg)
-		targetPath := getFinalProductRstPath(l.RstPath, item.Title)
-		fmt.Println(s)
-		writeToFile(targetPath, s)
-	}
-}
-
 // Get info of one final product
 func processTr(list productList, tr *goquery.Selection) productList {
 	item := productItem{}
---
+++ fetch/1sublist/type.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type productItem struct {
+	Title     string
+	Href      string
+	ImageSrcs []string
+}
+
+type productList struct {
+	RstPath string
+	Title   string
+	Url     string
+	Items   []productItem
+}
+
+func (l *productList) SetUrlTitle() {
+	_, err := replaceLines(l.RstPath, func(s string) string {
+		if strings.HasPrefix(s, ":title: ") {
+			l.Title = strings.TrimPrefix(s, ":title: ")
+		}
+		if strings.HasPrefix(s, ":source: ") {
+			l.Url = strings.TrimPrefix(s, ":source: ")
+		}
+		return s
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (l productList) CreateFinalProductRstFiles() {
+	fmt.Println()
+	for _, item := range l.Items {
+		ogImg := getRstImagePath(item.ImageSrcs[0])
+
+		s := rstMeta(item.Title, titleToSlug(item.Title),
+			"product, "+l.Title,
+			item.Title+" - "+l.Title,
+			"en", "", item.Href, ogImg)
+		targetPath := getFinalProductRstPath(l.RstPath, item.Title)
+		fmt.Println(s)
+		writeToFile(targetPath, s)
+	}
+}
