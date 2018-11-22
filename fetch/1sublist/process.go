279c42d431332dac4fcca657fc0a92cd1c2dc69d
siongui <siongui@gmail.com>
2018-11-23 04:50:40 +0800

    better code structure

--- fetch/1sublist/process.go
+++ fetch/1sublist/process.go
@@ -50,22 +50,25 @@ func createFinalProductRst(parentTitle, parentRstPath, title, href, src string) 
 }
 
 // Get info of one final product
-func processTr(parentUrl, parentTitle, parentRstPath string, tr *goquery.Selection) (rst string) {
+func processTr(list productList, tr *goquery.Selection) (rst string) {
+	item := productItem{}
+
 	// get title of the final product
 	a := tr.Find("a").First()
-	title := strings.TrimSpace(TrimSpaceNewlineInString(a.Text()))
+	item.Title = strings.TrimSpace(TrimSpaceNewlineInString(a.Text()))
 
 	// get url of the final product
 	href, ok := a.Attr("href")
 	if ok {
-		href = fullUrl(parentUrl, href)
+		item.Href = fullUrl(list.Url, href)
 	}
 
 	// get image link of the final product
 	img := tr.Find("img").First()
 	src, ok := img.Attr("src")
 	if ok {
-		src = fullUrl(parentUrl, src)
+		item.ImageSrcs = append(item.ImageSrcs, src)
+		src = fullUrl(list.Url, src)
 		downloadImage(src)
 	}
 
@@ -73,8 +76,8 @@ func processTr(parentUrl, parentTitle, parentRstPath string, tr *goquery.Selecti
 		sublistOgImage = ":og_image: " + getRstImagePath(src)
 	}
 
-	rst = titleImageToRstList(title, href, src)
-	createFinalProductRst(parentTitle, parentRstPath, title, href, src)
+	rst = titleImageToRstList(item.Title, item.Href, src)
+	createFinalProductRst(list.Title, list.RstPath, item.Title, item.Href, src)
 	return
 }
 
@@ -92,7 +95,7 @@ func handleProductList(list productList) {
 	table := doc.Find("#AutoNumber3").First()
 	// one iteration get the link of one final product
 	table.Find("tr").Each(func(_ int, tr *goquery.Selection) {
-		rstAll += processTr(list.Url, list.Title, list.RstPath, tr)
+		rstAll += processTr(list, tr)
 	})
 
 	// add og:image metadata
