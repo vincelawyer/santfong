7ae25d6f070da6725ddb309926cc6295d0969db3
siongui <siongui@gmail.com>
2018-11-27 07:39:59 +0800

    extractTablesHtml

--- fetch/1sublist/typetype.go
+++ fetch/1sublist/typetype.go
@@ -2,14 +2,51 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
+
+func isUnitLine(lineno int, line string) bool {
+	if strings.HasPrefix(line, `<p align="right"`) {
+		return true
+	}
+	return false
+}
+
+func isTableEnd(lineno int, line string) bool {
+	if line == `</table>` {
+		return true
+	}
+	return false
+}
+
+func extractTablesHtml(lines []string) (tables []string) {
+	table := ""
+	isInTable := false
+	for i, line := range lines {
+		if isUnitLine(i, line) {
+			isInTable = true
+		}
+		if isInTable {
+			table = table + line + "\n"
+		}
+		if isTableEnd(i, line) {
+			if isInTable == true {
+				isInTable = false
+				tables = append(tables, table)
+				table = ""
+			}
+		}
+	}
+	return
+}
 
 func parseTypeType(url string) {
 	lines, err := NonUtf8UrlToLines(url, "big5")
 	if err != nil {
 		panic(err)
 	}
-	for _, line := range lines {
-		fmt.Println(line)
-	}
+
+	tables := extractTablesHtml(lines)
+	fmt.Println(tables)
+	fmt.Println(len(tables))
 }
